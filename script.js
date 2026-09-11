ab18818b60512ca99b4b0867fd391a8bc74899d1
Ecl <142730570+EclCoder@users.noreply.github.com>
2026-09-11 21:51:11 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const title = document.getElementById("title");
+const button = document.getElementById("testButton");
+
+button.addEventListener("click", function () {
+    title.textContent = "JavaScript hoạt động!";
+});
